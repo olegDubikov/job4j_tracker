5a0f1654d2bb7ab14d82f41d3ef22374a56bb1db
hp <lars75@inbox.ru>
2022-09-26 19:52:18 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+}
