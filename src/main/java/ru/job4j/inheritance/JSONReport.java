4e12c41a7b30bb75125bbfed9d5c82b0b1115efa
hp <lars75@inbox.ru>
2022-10-01 00:41:10 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    String ln = System.lineSeparator();
+
+    public String generate(String name, String body) {
+        return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + "," + ln
+                + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
+    }
+}
