83dc7ab01017c029aba38932af7ab89ccded19d0
hp <lars75@inbox.ru>
2022-10-01 06:49:42 +0300

    3. Вызов конструктора родителя super(...) [#6837 #347552]

--- src/main/java/ru/job4j/inheritance/Profession.java
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -6,23 +6,4 @@ public class Profession {
     public Profession(boolean degree) {
         this.degree = degree;
     }
-
-    public class Engineer extends Profession {
-        private int experience;
-
-        public Engineer(int experience, boolean degree) {
-            super(degree);
-            this.experience = experience;
-        }
-    }
-
-    public class Programmer extends Engineer {
-
-        private String programLang;
-
-        public Programmer(String programLang, int experience, boolean degree) {
-            super(experience, degree);
-            this.programLang = programLang;
-        }
-    }
 }
