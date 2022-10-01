6b0d3a3e1ea2cf81051a0280593e275628d53ea1
hp <lars75@inbox.ru>
2022-10-01 21:42:05 +0300

    6. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
+    @Override
     public String generate(String name, String body) {
         return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + "," + ln
                 + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
