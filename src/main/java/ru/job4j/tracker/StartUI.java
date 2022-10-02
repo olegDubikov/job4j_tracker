469e76569711d25fc27f266bd39fbe9aa7d62962
hp <lars75@inbox.ru>
2022-10-02 13:16:11 +0300

    7. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        LocalDateTime item = item1.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String current = item.format(formatter);
+        System.out.print(current);
+    }
+}
