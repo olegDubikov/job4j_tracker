ad75dc53f3137dfc26a6266c06c6856734ba657f
hp <lars75@inbox.ru>
2022-10-02 12:55:59 +0300

    4. Переопределение [#174890 #347701]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,9 +1,11 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+
+    final String ln = System.lineSeparator();
+
     @Override
     public String generate(String name, String body) {
-        return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + "," + ln
-                + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
+        return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + "," + ln + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
     }
 }
