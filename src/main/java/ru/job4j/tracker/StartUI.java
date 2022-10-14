6272b0c0b32048500a4f40a93cb39bf4a2ea4c86
hp <lars75@inbox.ru>
2022-10-14 11:01:00 +0300

    2.6. Реализация класса StartUI. Удаление заявки [#500747 #351442]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -77,11 +77,7 @@ public class StartUI {
     }
 
     private void showMenu() {
-        String[] menu = {
-                "Add new Item", "Show all items", "Edit item",
-                "Delete item", "Find item by id", "Find items by name",
-                "Exit Program"
-        };
+        String[] menu = {"Add new Item", "Show all items", "Edit item", "Delete item", "Find item by id", "Find items by name", "Exit Program"};
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
