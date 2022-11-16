a25e04086d3cf15f0cb0d02e9bf471f43699f8b0
hp <lars75@inbox.ru>
2022-11-16 20:32:05 +0300

    4. Конвертация двумерного массива в ArrayList [#48580 #361468]

--- src/main/java/ru/job4j/collection/ConvertMatrix2List.java
+++ src/main/java/ru/job4j/collection/ConvertMatrix2List.java
@@ -1,4 +1,4 @@
-package ru.job4j.list;
+package ru.job4j.collection;
 
 
 import java.util.ArrayList;
--- src/test/java/ru/job4j/collection/ConvertMatrix2ListTest.java
+++ src/test/java/ru/job4j/collection/ConvertMatrix2ListTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.list;
+package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
+
 
 import java.util.Arrays;
 import java.util.List;
