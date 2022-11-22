ef7b36b5d79df5c328725cea2526402f5963b679
hp <lars75@inbox.ru>
2022-11-22 23:16:06 +0300

    3. Модели данных и HashSet. [#218530]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NotifyAccountTest {
+
+    @Test
+    public void whenTwoAccountsAddSent() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+
+    @Test
+    public void whenTwoAccountsEqualsOneRemove() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("123", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+}
