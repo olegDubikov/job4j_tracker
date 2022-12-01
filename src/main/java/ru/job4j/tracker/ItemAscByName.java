9d60003bfe2cf4b6136bb9b663ef7d6ce990c139
hp <lars75@inbox.ru>
2022-12-01 12:18:14 +0300

    0. Сортировка [#10096 #366328]

--- src/main/java/ru/job4j/tracker/ItemAscByName.java
+++ src/main/java/ru/job4j/tracker/ItemAscByName.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import java.util.Comparator;
 
 public class ItemAscByName implements Comparator<Item> {
-
     @Override
     public int compare(Item left, Item right) {
         return left.getName().compareTo(right.getName());
