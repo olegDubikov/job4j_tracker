7d321ee96369c93354bab722494329801a570f07
hp <lars75@inbox.ru>
2022-12-03 00:39:39 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/main/java/ru/job4j/collection/JobAscByPriority.java
@@ -0,0 +1,9 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class JobAscByPriority implements Comparator<Job> {
+    public int compare(Job o1, Job o2) {
+        return Integer.compare(o1.getPriority(), o2.getPriority());
+    }
+}
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import java.util.Comparator;
+import static org.assertj.core.api.Assertions.*;
+
+class JobTest {
+    @Test
+    public void whenComparatorDescByNameAndDescByPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 7),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByNameAndAscByPriority() {
+        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Fix bag", 3),
+                new Job("Fix bag", 7)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByName() {
+        Comparator<Job> cmpNameAsc = new JobAscByName();
+        int rsl = cmpNameAsc.compare(
+                new Job("Alisa", 2),
+                new Job("Henry", 2)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+Comparator<Job> cmpNameDesc = new JobDescByName();
+int rsl = cmpNameDesc.compare(
+        new Job("Ivan", 0),
+        new Job("Stepan", 1)
+);
+assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByPriority() {
+ Comparator<Job> cmpAscPriority = new JobAscByPriority();
+ int rsl = cmpAscPriority.compare(
+         new Job("Ivan", 1),
+         new Job("Ivan", 5)
+ );
+ assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+Comparator<Job> cmpDescPriority = new JobDescByName();
+int rsl = cmpDescPriority.compare(
+        new Job("Ivan", 3),
+        new Job("Ivan", 1)
+);
+assertThat(rsl).isGreaterThan(-1);
+    }
+}
