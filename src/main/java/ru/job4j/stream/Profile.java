f90b349962e21fcbf95e74205b71521568acc827
hp <lars75@inbox.ru>
2023-01-11 20:43:28 +0300

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Profile profile = (Profile) o;
+        return Objects.equals(address, profile.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
+    }
+}
