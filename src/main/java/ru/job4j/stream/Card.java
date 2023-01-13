d4385e1115a5f9cf78e199c2c58591b00e90c5dd
hp <lars75@inbox.ru>
2023-01-13 12:46:22 +0300

    9. Генерирование колоды карт. FlatMap [#504873 #380522]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -11,10 +11,15 @@ public class Card {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "Card " + suit + " " + value;
+    }
+
     public static void main(String[] args) {
         Stream.of(Suit.values())
                 .flatMap(value -> Stream.of(Value.values())
-                .map(suit -> value + " " + suit))
+                        .map(suit -> new Card(value, suit)))
                 .forEach(System.out::println);
     }
 }
