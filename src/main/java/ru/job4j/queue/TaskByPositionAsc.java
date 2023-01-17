ba7ec8e38eebc153cc3ee5f5dc73970a8e62fb51
hp <lars75@inbox.ru>
2023-01-17 21:27:29 +0300

    5. PriorityQueue [#504944 #381791]

--- src/main/java/ru/job4j/queue/TaskByPositionAsc.java
+++ src/main/java/ru/job4j/queue/TaskByPositionAsc.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class TaskByPositionAsc implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        return Integer.compare(o1.position().ordinal(), o2.position().ordinal());
+        return o1.position().compareTo(o2.position());
     }
 }
